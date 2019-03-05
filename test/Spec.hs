30ea2ba3ca6b511147725d7caccdad6e5b2a3e07
Dmitrij Pavluk <al.lastor@bk.ru>
2019-03-05 21:52:35 +0300

    parsing of chanel WIP

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,23 +10,25 @@ isOk :: IO Bool -> Expectation
 isOk action = shouldReturn action True
 
 main :: IO ()
-main = hspec $ do
-    it "Parsing of record 1" $ shouldReturn
-        (pure $ parse record "" " <div><span>14:00</span><span><em>Kauhea kankkunen</em></span></div>")
-        (Right $ Record "14:00" "Kauhea kankkunen")
+main = do
+    putTextLn ""
+    hspec $ do
+        it "Parsing of record 1" $ shouldReturn
+            (pure $ parse record "" " <div><span>14:00</span><span><em>Kauhea kankkunen</em></span></div>")
+            (Right $ Record "14:00" "Kauhea kankkunen")
 
-    it "Parsing of record 2" $ shouldReturn
-        (pure $ parse record "" " <div><span>14:00</span><span>Kauhea kankkunen</span></div>")
-        (Right $ Record "14:00" "Kauhea kankkunen")
+        it "Parsing of record 2" $ shouldReturn
+            (pure $ parse record "" " <div><span>14:00</span><span>Kauhea kankkunen</span></div>")
+            (Right $ Record "14:00" "Kauhea kankkunen")
 
-    it "Parsing of channel" $ shouldReturn
-        (pure $ parse channel "" "<div id=\"yle2\"><h3>Yle 2</h3> <div><span>02:00</span><span>Yle uutiset</span></div><div><span>03:00</span><span>Yle uutiset</span></div></div>")
-        (Right $ Channel "Yle 2" [Record "02:00" "Yle uutiset", Record "03:00" "Yle uutiset"])
-        
-    it "Parsing of record name1" $ shouldReturn
-        (pure $ parse recordName "" "Kauhea kankkunen")
-        (Right "Kauhea kankkunen")
+        it "Parsing of channel" $ shouldReturn
+            (pure $ parse channel "" "<div id=\"yle2\"><h3>Yle 2</h3> <div><span>02:00</span><span>Yle uutiset</span></div><div><span>03:00</span><span>Yle uutiset</span></div></div>")
+            (Right $ Channel "Yle 2" [Record "02:00" "Yle uutiset", Record "03:00" "Yle uutiset"])
+            
+        it "Parsing of record name1" $ shouldReturn
+            (pure $ parse recordName "" "Kauhea kankkunen")
+            (Right "Kauhea kankkunen")
 
-    it "Parsing of record name2" $ shouldReturn
-        (pure $ parse recordName "" "<em>Kauhea kankkunen</em>")
-        (Right "Kauhea kankkunen")
+        it "Parsing of record name2" $ shouldReturn
+            (pure $ parse recordName "" "<em>Kauhea kankkunen</em>")
+            (Right "Kauhea kankkunen")
