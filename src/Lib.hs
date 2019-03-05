409cffe582aa3928a014e47a6a003d5932d348ba
Dmitrij Pavluk <al.lastor@bk.ru>
2019-03-05 20:48:47 +0300

    parsing of chanel WIP

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,27 +2,53 @@
 module Lib
     ( someFunc
     , record
+    , channel
     , Record(..)
-    , test
+    , Channel(..)
     ) where
 
-import Universum hiding (many, optional)
-import Text.Parsec
+import Universum hiding (many, optional, (<|>))
+import Text.Parsec 
+import Text.Parsec.Combinator
 import Text.Parsec.Char
 import Data.Text (pack)
-
-data Record = Record Text Text deriving (Show, Eq)
 
 someFunc :: IO ()
 someFunc = putTextLn "someFunc"
 
+data Channel = Channel Text [Record] deriving (Show, Eq)
+
+channel :: Parsec Text u Channel
+channel = {- between (do
+        string "<div id=\""
+        void $ many $ noneOf "\""
+        string "\">"
+    ) (string "</div>"
+    ) -} (do
+        name    <- channelName
+        records <- many record
+        pure $ Channel name records
+    )
+
+channelName :: Parsec Text u Text
+channelName = do
+    name  <- between (string "<h3>") (string "</h3>") (many $ noneOf "<>")
+    pure $ pack name
+
+data Record = Record Text Text deriving (Show, Eq)
+
 record :: Parsec Text u Record
-record = do
-    string "<div><span>"
-    time <- many $ noneOf "<>"
-    string "</span><span>"
-    optional $ string "<em>"
-    name <- many $ noneOf "<>"
-    optional $ string "</em>"
-    string "</span></div>"
-    pure $ Record (pack time) (pack name)
+record = between (string "<div>") (string "</div>") $ do
+    time <- between (string "<span>") (string "</span>") (many $ noneOf "<>")
+    name <- between (string "<span>") (string "</span>") recordName
+    pure $ Record (pack time) name
+
+recordName :: Parsec Text u Text
+recordName =
+    (do
+        name <- between (string "<em>") (string "</em>") $ many (noneOf "<>")
+        pure $ pack name
+    ) <|> (do
+        name <- many $ noneOf "<>"
+        pure $ pack name
+    )  
--- test/Spec.hs
+++ test/Spec.hs
@@ -10,7 +10,16 @@ isOk :: IO Bool -> Expectation
 isOk action = shouldReturn action True
 
 main :: IO ()
-main = hspec $
+main = hspec $ do
     it "Parsing of record" $ shouldReturn
         (pure $ parse record "" "<div><span>14:00</span><span><em>Kauhea kankkunen</em></span></div>")
         (Right $ Record "14:00" "Kauhea kankkunen")
+
+    it "Parsing of record" $ shouldReturn
+        (pure $ parse record "" "<div><span>14:00</span><span>Kauhea kankkunen</span></div>")
+        (Right $ Record "14:00" "Kauhea kankkunen")
+
+    it "Parsing of channel" $ shouldReturn
+        (pure $ parse channel "" "<h3>Yle 2</h3><div><span>02:00</span><span>Yle uutiset</span></div><div><span>03:00</span><span>Yle uutiset</span></div>")
+        (Right $ Channel "Yle 2" [Record "02:00" "Yle uutiset", Record "03:00" "Yle uutiset"])
+        
