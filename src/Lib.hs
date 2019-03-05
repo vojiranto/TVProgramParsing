2550a802b8f1e5b66f449871abf991d0f47121e1
Dmitrij Pavluk <al.lastor@bk.ru>
2019-03-05 19:48:25 +0300

    parsing of records

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
     ( someFunc
+    , record
+    , Record(..)
+    , test
     ) where
 
+import Universum hiding (many, optional)
+import Text.Parsec
+import Text.Parsec.Char
+import Data.Text (pack)
+
+data Record = Record Text Text deriving (Show, Eq)
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putTextLn "someFunc"
+
+test =  parse record "" "<div><span>14:00</span><span><em>Kauhea kankkunen</em></span></div>"
+
+record :: Parsec Text u Record
+record = do
+    string "<div><span>"
+    time <- many $ noneOf "<>"
+    string "</span><span>"
+    optional $ string "<em>"
+    name <- many $ noneOf "<>"
+    optional $ string "</em>"
+    string "</span></div>"
+    pure $ Record (pack time) (pack name)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import           Universum
+
+import           Test.Hspec
+import           Text.Parsec
+import           Lib
+
+isOk :: IO Bool -> Expectation
+isOk action = shouldReturn action True
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+    it "Parsing of record" $ shouldReturn
+        (pure $ parse record "" "<div><span>14:00</span><span><em>Kauhea kankkunen</em></span></div>")
+        (Right $ Record "14:00" "Kauhea kankkunen")
