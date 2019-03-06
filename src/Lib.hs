d27f0bb2611790c5d9a1b8e433d62d361d631d6c
Dmitrij Pavluk <al.lastor@bk.ru>
2019-03-06 10:13:50 +0300

    day

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,11 +4,13 @@ module Lib
     ( someFunc
     , record
     , channel
+    , day
     , tag
     , bString
     , recordName
     , Record(..)
     , Channel(..)
+    , Day (..)
     ) where
 
 import Universum hiding (many, optional, (<|>), try)
@@ -21,30 +23,52 @@ import Data.Text (pack)
 someFunc :: IO ()
 someFunc = putTextLn "someFunc"
 
+content :: Parsec Text u [Day]
+content =
+    tag "html" $ do
+        tag "head" spaces
+        tag "body" $ do
+            string "<div id=\"content\">"
+            manyUntil day "</div>"
+
+data Day = Day [Channel] deriving (Show, Eq)
+
+day :: Parsec Text u Day
+day = do
+    spaces >> string "<div class=\"day\" id=\"" >> skipMany (noneOf "\"") >> string "\">"
+    void (tag "h2" bString)
+    Day <$> manyUntil channel "</div>"
+
 data Channel = Channel Text [Record] deriving (Show, Eq)
 
 channel :: Parsec Text u Channel
 channel = do
     spaces >> string "<div id=\"" >> skipMany (noneOf "\"") >> string "\">"
-
-    name    <- channelName
-    records <- manyTill record (try (string "</div>"))
-    pure $ Channel name records
+    Channel <$> channelName <*> manyUntil record "</div>"
 
 channelName :: Parsec Text u Text
-channelName = spaces >> tag "h3" bString
+channelName = tag "h3" bString
 
 data Record = Record Text Text deriving (Show, Eq)
 
 record :: Parsec Text u Record
-record = 
-    spaces >> tag "div" (Record <$> tag "span" bString <*> tag "span" recordName)   
+record = tag "div" (Record <$> tag "span" bString <*> tag "span" recordName)   
 
 recordName :: Parsec Text u Text
 recordName = tag "em" bString <|> bString
 
+--------------------------------------------------------------------------------
+--                              Common Internal                               --
+--------------------------------------------------------------------------------
+
 bString :: Parsec Text u Text
 bString = pack <$> (many $ noneOf "<")
 
+
 tag :: String -> Parsec Text u p -> Parsec Text u p
-tag n = between (string $ "<" <> n <> ">") (string $ "</" <> n <> ">")
+tag n p = do
+    spaces
+    between (string $ "<" <> n <> ">") (string $ "</" <> n <> ">") p
+
+manyUntil :: Parsec Text u a -> String -> Parsec Text u [a]
+manyUntil p t = manyTill p (try (spaces >> string t))
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
-
+{-# LANGUAGE QuasiQuotes       #-}
 import           Universum
 
 import           Test.Hspec
 import           Text.Parsec
+import           Text.RawString.QQ
 import           Lib
 
 isOk :: IO Bool -> Expectation
@@ -21,10 +22,28 @@ main = do
             (pure $ parse record "" " <div><span>14:00</span><span>Kauhea kankkunen</span></div>")
             (Right $ Record "14:00" "Kauhea kankkunen")
 
+        it "Parsing of day" $ shouldReturn
+            (pure $ parse day "" [r|
+                <div class="day" id="monday">
+                    <h2>Monday</h2>
+                    <div id="yle1">
+                        <h3>Yle 1</h3>
+                            <div><span>02:00</span><span>Yle uutiset</span></div>
+                    </div>
+                </div>
+            |])
+            (Right $ Day [Channel "Yle 1" [Record "02:00" "Yle uutiset"]])
+
         it "Parsing of channel" $ shouldReturn
-            (pure $ parse channel "" "<div id=\"yle2\"><h3>Yle 2</h3> <div><span>02:00</span><span>Yle uutiset</span></div><div><span>03:00</span><span>Yle uutiset</span></div></div>")
+            (pure $ parse channel "" [r|
+                <div id="yle2">
+                    <h3>Yle 2</h3>
+                    <div><span>02:00</span><span>Yle uutiset</span></div>
+                    <div><span>03:00</span><span>Yle uutiset</span></div>
+                </div>
+            |])
             (Right $ Channel "Yle 2" [Record "02:00" "Yle uutiset", Record "03:00" "Yle uutiset"])
-            
+    
         it "Parsing of record name1" $ shouldReturn
             (pure $ parse recordName "" "Kauhea kankkunen")
             (Right "Kauhea kankkunen")
